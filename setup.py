4bafc5dc745d2033604ec7c0d2cea93b2f3731ad
Matthew Dezzi <matthew.dezzi@proconexdirect.com>
2026-03-17 10:14:18 -0400

    reorganized into a proper package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ifsrequest",              
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=['requests','urllib.parse'],           # add dependencies here
+    author="Matthew Dezzi",
+    description="Requests wrapper that will handle IFS token retrieval automatically",
+    python_requires=">=3.7",
+)
