8f35605e994998b745889fc6d96296206cbf895c
Matthew Dezzi <matthew.dezzi@proconexdirect.com>
2026-06-12 09:24:22 -0400

    Added typing

--- ifsrequest/ifsrequest.py
+++ ifsrequest/ifsrequest.py
@@ -1,19 +1,21 @@
-import requests, logging
+import logging
 from urllib.parse import urljoin
+import requests
+from typing import Any, Dict, Optional
+from requests import Response
 
 logger = logging.getLogger(__name__)
 
 class IfsRequest:
-    def __init__(self, base_url, auth):
+    def __init__(self, base_url: str, auth: Dict[str, Any]) -> None:
         self.base_url = base_url
 
         auth_type = auth.get('type')
-        self.auth_type = auth_type.lower()
-        
         if not auth_type:
             raise ValueError('Did not receive auth type.')
-        
-        if auth_type.lower() == 'client':
+        self.auth_type = auth_type.lower()
+
+        if self.auth_type == 'client':
             logger.debug('Creating CLIENT auth object')
             client_secret = auth.get('client_secret')
             client_id = auth.get('client_id')
@@ -24,7 +26,7 @@ class IfsRequest:
             self.client_secret = client_secret
             self.realm = realm
             self.token = self._get_ifs_token()
-        elif auth_type.lower() == 'user':
+        elif self.auth_type == 'user':
             username = auth.get('username')
             password = auth.get('password')
             if not (username and password):
@@ -32,125 +34,128 @@ class IfsRequest:
             self.username = username
             self.password = password
         else:
-            raise ValueError(f'Unable to create IFS Auth with auth type [{auth_type.lower()}]')
+            raise ValueError(f'Unable to create IFS Auth with auth type [{self.auth_type}]')
 
-    def _get_ifs_token(self):
-        token_data={"grant_type": "client_credentials",
-                    "client_id":self.client_id,
-                    "client_secret":self.client_secret}
-        token_url = urljoin(self.base_url,f'/auth/realms/{self.realm}/protocol/openid-connect/token')
+    def _get_ifs_token(self) -> str:
+        token_data = {
+            "grant_type": "client_credentials",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+        }
+        token_url = urljoin(self.base_url, f'/auth/realms/{self.realm}/protocol/openid-connect/token')
         logger.debug('Requesting token from IFS')
         r = requests.post(token_url, data=token_data)
         if r.status_code == 200:
             token = r.json().get('access_token')
             logger.debug('Token received')
+            return token
         else:
             logger.error(f'Did not receive token from IFS. URL: {token_url} Data: {token_data}')
             raise ValueError('Unable to retrieve IFS token. Please check credentials.')
-        return token
-    
-    def _check_ifs_token(self):
+
+    def _check_ifs_token(self) -> bool:
         api_url = urljoin(self.base_url, "/main/ifsapplications/projection/v1/UserSettings.svc/SingletonUser")
         r = requests.get(api_url)
-        if r.status_code == 200:
-            return True
-        else:
-            return False
-    
-    def _handle_url(self, url):
+        return r.status_code == 200
+
+    def _handle_url(self, url: str) -> str:
         if self.base_url not in url:
             # checking if it is projection only
             if f'main/ifsapplications/projection/v1' not in url:
-                url = urljoin(f'{self.base_url}/main/ifsapplications/projection/v1/',url)
+                url = urljoin(f'{self.base_url}/main/ifsapplications/projection/v1/', url)
             else:
-                url = urljoin(self.base_url, url) 
+                url = urljoin(self.base_url, url)
         return url
 
-    def get(self, url, **kwargs):
+    def get(self, url: str, **kwargs: Any) -> Response:
         url = self._handle_url(url)
         if self.auth_type == 'client':
             attempts = 0
             while attempts < 3:
-                headers = kwargs.get('headers',{}) | {'Authorization':f'Bearer {self.token}'}
+                headers = kwargs.get('headers', {}) | {'Authorization': f'Bearer {self.token}'}
                 r = requests.get(url, headers=headers)
                 if r.status_code == 401:
-                    logger.warning(f'Received 401 from IFS. Attempting to retreive new token.')
+                    logger.warning('Received 401 from IFS. Attempting to retrieve new token.')
                     self.token = self._get_ifs_token()
                     attempts += 1
                 else:
                     return r
-            else:
-                logger.warning('Reached maximum retries')
-                return r
+            logger.warning('Reached maximum retries')
+            return r
         elif self.auth_type == 'user':
-            headers = kwargs.get('headers',{})
+            headers = kwargs.get('headers', {})
             return requests.get(url, headers=headers, auth=(self.username, self.password))
+        else:
+            raise RuntimeError('Unsupported auth type')
 
-    def post(self, url, **kwargs):
+    def post(self, url: str, **kwargs: Any) -> Response:
         url = self._handle_url(url)
         if self.auth_type == 'client':
             attempts = 0
             while attempts < 3:
-                headers = kwargs.get('headers',{}) | {'Authorization':f'Bearer {self.token}'}
-                json = kwargs.get('json',{})
+                headers = kwargs.get('headers', {}) | {'Authorization': f'Bearer {self.token}'}
+                json = kwargs.get('json', {})
                 r = requests.post(url, json=json, headers=headers) if json else requests.post(url, headers=headers)
                 if r.status_code == 401:
-                    logger.warning(f'Received 401 from IFS. Attempting to retreive new token.')
+                    logger.warning('Received 401 from IFS. Attempting to retrieve new token.')
                     self.token = self._get_ifs_token()
                     attempts += 1
                 else:
                     return r
-            else:
-                logger.warning('Reached maximum retries')
-                return r
+            logger.warning('Reached maximum retries')
+            return r
         elif self.auth_type == 'user':
-            headers = kwargs.get('headers',{})
-            json = kwargs.get('json',{})
+            headers = kwargs.get('headers', {})
+            json = kwargs.get('json', {})
             r = requests.post(url, json=json, headers=headers, auth=(self.username, self.password)) if json else requests.post(url, headers=headers, auth=(self.username, self.password))
             return r
-    
-    def patch(self, url, **kwargs):
+        else:
+            raise RuntimeError('Unsupported auth type')
+
+    def patch(self, url: str, **kwargs: Any) -> Response:
         url = self._handle_url(url)
         if self.auth_type == 'client':
             attempts = 0
             while attempts < 3:
-                headers = kwargs.get('headers',{}) | {'Authorization':f'Bearer {self.token}'}
-                json = kwargs.get('json',{})
+                headers = kwargs.get('headers', {}) | {'Authorization': f'Bearer {self.token}'}
+                json = kwargs.get('json', {})
                 r = requests.patch(url, json=json, headers=headers) if json else requests.patch(url, headers=headers)
                 if r.status_code == 401:
-                    logger.warning(f'Received 401 from IFS. Attempting to retreive new token.')
+                    logger.warning('Received 401 from IFS. Attempting to retrieve new token.')
                     self.token = self._get_ifs_token()
                     attempts += 1
                 else:
                     return r
-            else:
-                logger.warning('Reached maximum retries')
-                return r
+            logger.warning('Reached maximum retries')
+            return r
         elif self.auth_type == 'user':
-            headers = kwargs.get('headers',{})
-            json = kwargs.get('json',{})
+            headers = kwargs.get('headers', {})
+            json = kwargs.get('json', {})
             r = requests.patch(url, json=json, headers=headers, auth=(self.username, self.password)) if json else requests.patch(url, headers=headers, auth=(self.username, self.password))
             return r
+        else:
+            raise RuntimeError('Unsupported auth type')
 
-    def delete(self, url, **kwargs):
+    def delete(self, url: str, **kwargs: Any) -> Response:
         url = self._handle_url(url)
         if self.auth_type == 'client':
             attempts = 0
             while attempts < 3:
-                headers = kwargs.get('headers',{}) | {'Authorization':f'Bearer {self.token}'}
-                json = kwargs.get('json',{})
+                headers = kwargs.get('headers', {}) | {'Authorization': f'Bearer {self.token}'}
+                json = kwargs.get('json', {})
                 r = requests.delete(url, json=json, headers=headers) if json else requests.delete(url, headers=headers)
                 if r.status_code == 401:
-                    logger.warning(f'Received 401 from IFS. Attempting to retreive new token.')
+                    logger.warning('Received 401 from IFS. Attempting to retrieve new token.')
                     self.token = self._get_ifs_token()
                     attempts += 1
                 else:
                     return r
-            else:
-                logger.warning('Reached maximum retries')
-                return r
+            logger.warning('Reached maximum retries')
+            return r
         elif self.auth_type == 'user':
-            headers = kwargs.get('headers',{})
-            json = kwargs.get('json',{})
+            headers = kwargs.get('headers', {})
+            json = kwargs.get('json', {})
             r = requests.delete(url, json=json, headers=headers, auth=(self.username, self.password)) if json else requests.delete(url, headers=headers, auth=(self.username, self.password))
             return r
+        else:
+            raise RuntimeError('Unsupported auth type')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ifsrequest",              
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(),
     install_requires=['requests'],  
     author="Matthew Dezzi",
